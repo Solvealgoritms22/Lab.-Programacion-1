959d3e42196c95f6d082d737c1ad0c01fb8b35a9
Solvealgoritms22 <112198161+Solvealgoritms22@users.noreply.github.com>
2022-11-21 23:31:51 -0400

    Add files via upload

--- Herencia.cs
+++ Herencia.cs
@@ -1,22 +1,17 @@
 using System;
 
-public class G:F
-{
-    private int s=9998833;
-    public int gets(){
-        return this.s;
-    }
-}
-
-public class F
+public class F:G
 {
     private string x="Ferreiras, Castro";
     private double y=12.34E+12;
     private long z=987364354;
     protected byte w=34;
     
-    public F(){
-
+    public F(string x, double y, long z, byte w){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+        this.w=w;
     }
 
     public string getX(){
@@ -38,18 +33,22 @@ public class F
        Console.WriteLine(getX());
        Console.WriteLine(getY());
        Console.WriteLine(getZ());
-       G Objeto1 = new G();
-       Console.WriteLine(Objeto1.gets());
 
     }
 
 }
 
+public class G
+{
+    private int s=98888833;
+    
+}
+
 public class TesterClass
 {
     public static void Main(String[] args){
-
-        F Objeto1 = new F();
+        
+        F Objeto1 = new F("Darling, Fajardo",13.34E+12,999364354,50);
         Objeto1.verObjeto();
         
     }
